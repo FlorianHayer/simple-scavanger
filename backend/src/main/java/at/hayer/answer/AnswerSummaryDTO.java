e52268f0d94301d5a6e5c5e87503f8553368d768
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2026-06-07 22:17:18 +0200

    Add ID field to AnswerSummaryDTO and QuestionSummaryDTO for better entity representation

--- backend/src/main/java/at/hayer/answer/AnswerSummaryDTO.java
+++ backend/src/main/java/at/hayer/answer/AnswerSummaryDTO.java
@@ -1,7 +1,7 @@
 package at.hayer.answer;
 
-public record AnswerSummaryDTO(String text, boolean correct) {
+public record AnswerSummaryDTO(Long id, String text, boolean correct) {
     public static AnswerSummaryDTO fromEntity(Answer answer) {
-        return new AnswerSummaryDTO(answer.text, answer.correct);
+        return new AnswerSummaryDTO(answer.id, answer.text, answer.correct);
     }
 }
--- backend/src/main/java/at/hayer/question/QuestionSummaryDTO.java
+++ backend/src/main/java/at/hayer/question/QuestionSummaryDTO.java
@@ -4,9 +4,9 @@ import at.hayer.answer.AnswerSummaryDTO;
 
 import java.util.List;
 
-public record QuestionSummaryDTO(String text, String location, List<AnswerSummaryDTO> answers) {
+public record QuestionSummaryDTO(Long id, String text, String location, List<AnswerSummaryDTO> answers) {
     public static QuestionSummaryDTO fromEntity(Question question) {
-        return new QuestionSummaryDTO(question.text, question.location, question.answers
+        return new QuestionSummaryDTO(question.id, question.text, question.location, question.answers
                 .stream()
                 .map(AnswerSummaryDTO::fromEntity)
                 .toList());
